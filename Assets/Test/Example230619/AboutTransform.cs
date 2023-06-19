355a0f16c9a8696aba2ddd36d6c56a36faf27c5e
whwogus947 <78160815+whwogus947@users.noreply.github.com>
2023-06-19 13:52:54 +0900

    About transform.

---
+++ Assets/Test/Example230619/AboutTransform.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AboutTransform : MonoBehaviour
+{
+    //유니티에서 Transform
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
