e43ca9a4dc2ca9414e1b5101ed95b869430ac671
whwogus947 <78160815+whwogus947@users.noreply.github.com>
2023-06-19 14:23:28 +0900

    Example 230619

--- Assets/Test/Example230619/AboutTransform.cs
+++ Assets/Test/Example230619/AboutTransform.cs
@@ -1,19 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO.Compression;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AboutTransform : MonoBehaviour
 {
-    //유니티에서 Transform
-    // Start is called before the first frame update
+    // Unity -> Transform : Position, Rotation, Scale...
+    // Transform -> Must have item.
+    // GetComponent
+    public Transform parent;
+
+    // caching
+    //public CanvasScaler scaler;
+
+    private CanvasScaler scaler;
+
     void Start()
     {
-        
+        //transform.SetParent(parent);
+        //transform.SetSiblingIndex(0);
+        //transform.parent = parent;
+        //transform.root.gameObject.SetActive(false);
+        //transform.parent = null;
+
+        //scaler = transform.parent.GetComponent<CanvasScaler>();
+        //scaler = transform.GetComponentInParent<CanvasScaler>();
+        //scaler = transform.GetComponentInChildren<CanvasScaler>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    // 관통되는 충돌 (Physics X)
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+    }
+
+    // 관통되지 않는 충돌 (Physics O)
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //collision has no tag or name
+        //collision's gameObject have it
+        collision.transform.GetComponent<AudioSource>().Play();
+
+
     }
 }
