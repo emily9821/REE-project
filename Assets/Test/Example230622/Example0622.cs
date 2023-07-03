dd644300e86d26d9fb599468d00fb2081ddfb6c1
whwogus947 <78160815+whwogus947@users.noreply.github.com>
2023-07-03 15:00:52 +0900

    Update Example0622.cs

--- Assets/Test/Example230622/Example0622.cs
+++ Assets/Test/Example230622/Example0622.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class Example0622 : MonoBehaviour
 {
+    public string collidedObjectName;
+    public HOUSE_OBJECT detected;
+    //collidedObject = 1
+    // 1 -> TV
+    // 2 -> COMPUTER
+    // 3 -> RADIO
     // Start is called before the first frame update
     void Start()
     {
@@ -15,5 +21,24 @@ public class Example0622 : MonoBehaviour
     {
         Debug.Log(transform.right);
         Debug.DrawLine(transform.position, transform.forward);
+
+
+        //GetKeyDown -> Every frame
+        if (Input.GetKeyDown(KeyCode.Space) && detected == HOUSE_OBJECT.TV)
+        {
+            // DO SOMETHING.
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        detected = HOUSE_OBJECT.TV;
+    }
+
+    public enum HOUSE_OBJECT
+    {
+        TV,
+        COMPUTER,
+        RADIO,
     }
 }
